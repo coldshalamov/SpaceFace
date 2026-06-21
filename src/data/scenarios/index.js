8ed3c56d056502796f6fcb6916df47d518c44460
coldshalamov <93robingattis@gmail.com>
2026-06-21 00:05:18 -0400

    SG-05: register declarative scenarios

---
+++ src/data/scenarios/index.js
@@ -0,0 +1,5 @@
+import { SHIPMENT_47A } from './shipment47a.js';
+
+export { SHIPMENT_47A } from './shipment47a.js';
+export const SCENARIOS = Object.freeze([SHIPMENT_47A]);
+export const SCENARIO_BY_ID = new Map(SCENARIOS.map((scenario) => [scenario.id, scenario]));
